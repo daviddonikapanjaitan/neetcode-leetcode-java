891d2093c62d97b00a8df2f4ab3073e2f826b37f
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-05-14 23:19:03 +0700

    add solutions for tree problems.

---
+++ src/C7Trees/P15SerializeAndDeserializeBinaryTree/Solution.java
@@ -0,0 +1,11 @@
+package C7Trees.P15SerializeAndDeserializeBinaryTree;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public Solution(){
+    }
+}
---
+++ src/Practice/C7Trees/P15SerializeAndDeserializeBinaryTree/Solution.java
@@ -0,0 +1,4 @@
+package Practice.C7Trees.P15SerializeAndDeserializeBinaryTree;
+
+public class Solution {
+}
