33439b9f7d9c0ccf57ca1d13f081d249bfb40b6f
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-05-15 22:06:13 +0700

    add solutions for tree problems.

--- src/C7Trees/P15SerializeAndDeserializeBinaryTree/Solution.java
+++ src/C7Trees/P15SerializeAndDeserializeBinaryTree/Solution.java
@@ -1,11 +1,83 @@
 package C7Trees.P15SerializeAndDeserializeBinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
     public static void main(String[] args) {
         new Solution();
     }
 
-    public Solution(){
+    public Solution() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+        String resultString = serialize(root);
+        System.out.println(resultString);
+        TreeNode deserialization = deserialize(resultString);
+        System.out.println(deserialization.val);
+        System.out.println(deserialization.left.val);
+        System.out.println(deserialization.right.val);
+    }
+
+    public String serialize(TreeNode root) {
+        if(root == null) return "N";
+        StringBuilder res = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if(node == null) {
+                res.append("N,");
+            } else {
+                res.append(node.val).append(",");
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        return res.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] vals = data.split(",");
+        if(vals[0].equals("N")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int index = 1;
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(!vals[index].equals("N")){
+                node.left = new TreeNode(Integer.parseInt(vals[index]));
+                queue.add(node.left);
+            }
+            index++;
+            if(!vals[index].equals("N")){
+                node.right = new TreeNode(Integer.parseInt(vals[index]));
+                queue.add(node.right);
+            }
+            index++;
+        }
+
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
