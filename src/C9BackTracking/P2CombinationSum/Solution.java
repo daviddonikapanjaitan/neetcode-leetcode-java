848c5938ab6657e837f0d49ab5b3521d3f316516
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-05-25 03:20:37 +0700

    add solutions for tree problems.

---
+++ src/C9BackTracking/P2CombinationSum/Solution.java
@@ -0,0 +1,49 @@
+package C9BackTracking.P2CombinationSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    List<List<Integer>> res;
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public Solution() {
+        // int[] candidates = new int[]{2,3,6,7};
+        // int target = 7;
+
+        int[] candidates = new int[]{2,3,5};
+        int target = 8;
+
+        // int[] candidates = new int[]{2};
+        // int target = 1;
+
+        List<List<Integer>> result = combinationSum(candidates, target);
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        res = new ArrayList<List<Integer>>();
+        List<Integer> cur = new ArrayList<>();
+        backtrack(nums, target, cur, 0);
+        return res;
+    }
+
+    public void backtrack(int[] nums, int target, List<Integer> cur, int i) {
+        if(target == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        if(target < 0 || i >= nums.length) {
+            return;
+        }
+        cur.add(nums[i]);
+        backtrack(nums, target - nums[i], cur, i);
+        cur.remove(cur.size() - 1);
+        backtrack(nums, target, cur, i + 1);
+    }
+}
---
+++ src/Practice/C9BackTracking/P1Subsets/Solution.java
@@ -0,0 +1,41 @@
+package Practice.C9BackTracking.P1Subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public Solution() {
+        // int[] nums = new int[]{1,2,3};
+        // List<List<Integer>> result = subsets(nums);
+
+        int[] nums = new int[]{0};
+        List<List<Integer>> result = subsets(nums);
+
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ree = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        dfs(nums, 0, subset, ree);
+
+        return ree;
+    }
+
+    private void dfs(int[] nums, int i, List<Integer> subset, List<List<Integer>> res) {
+        if(i >= nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[i]);
+        dfs(nums, i + 1, subset, res);
+        subset.remove(subset.size() - 1);
+        dfs(nums, i + 1, subset, res);
+    }
+}
