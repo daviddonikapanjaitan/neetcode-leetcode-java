201ec6e3da971da6c8d10a53accf881b52918cd6
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-05-27 01:50:09 +0700

    add solutions for tree problems.

---
+++ src/Practice/C9BackTracking/P9NQueens/Solution.java
@@ -0,0 +1,61 @@
+package Practice.C9BackTracking.P9NQueens;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public Solution() {
+        int n = 1;
+        List<List<String>> result = solveNQueens(n);
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backTrack(0, board, res);
+        return res;
+    }
+
+    private void backTrack(int r, char[][] board, List<List<String>> res) {
+        if(r == board.length) {
+            List<String> copy = new ArrayList<>();
+            for(char[] row: board) {
+                copy.add(new String(row));
+            }
+            res.add(copy);
+            return;
+        }
+        for (int c = 0; c < board.length; c++) {
+            if(isSafe(r, c, board)) {
+                board[r][c] = 'Q';
+                backTrack(r + 1, board, res);
+                board[r][c] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(int r, int c, char[][] board) {
+        for (int i = r - 1; i >= 0; i--) {
+            if(board[i][c] == 'Q') return false;
+        }
+        for (int i = r - 1, j = c - 1; i >= 0 && j >= 0 ; i--, j--) {
+            if(board[i][j] == 'Q') return false;
+        }
+        for (int i = r - 1, j = c + 1; i >= 0 && j < board.length; i--, j++) {
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+}
