5e383c73e90e974ed83e0040825cac50bb2a81dc
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-05-27 21:47:52 +0700

    add solutions for tree problems.

--- src/C10HeapPriorityQueue/P1KthLargestElementInAStream/Solution.java
+++ src/C10HeapPriorityQueue/P1KthLargestElementInAStream/Solution.java
@@ -1,50 +1,53 @@
 package C10HeapPriorityQueue.P1KthLargestElementInAStream;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class Solution {
-    List<Integer> arr;
-    int K;
+    private PriorityQueue<Integer> minHeap;
+    private int k;
 
     public static void main(String[] args) {
-//        Solution solution = new Solution(3, new int[]{4,5,8,2});
-//        int one = solution.add(3);
-//        int two = solution.add(5);
-//        int three = solution.add(10);
-//        int four = solution.add(9);
-//        int five = solution.add(4);
-//
-//        System.out.println(one);
-//        System.out.println(two);
-//        System.out.println(three);
-//        System.out.println(four);
-//        System.out.println(five);
-
-        Solution solution = new Solution(4, new int[]{7,7,7,7,8,3});
-        int one = solution.add(2);
-        int two = solution.add(10);
-        int three = solution.add(9);
+        Solution solution = new Solution(3, new int[]{4,5,8,2});
+        int one = solution.add(3);
+        int two = solution.add(5);
+        int three = solution.add(10);
         int four = solution.add(9);
+        int five = solution.add(4);
 
         System.out.println(one);
         System.out.println(two);
         System.out.println(three);
         System.out.println(four);
+        System.out.println(five);
+
+//        Solution solution = new Solution(4, new int[]{7,7,7,7,8,3});
+//        int one = solution.add(2);
+//        int two = solution.add(10);
+//        int three = solution.add(9);
+//        int four = solution.add(9);
+//
+//        System.out.println(one);
+//        System.out.println(two);
+//        System.out.println(three);
+//        System.out.println(four);
     }
 
     public Solution(int k, int[] nums) {
-        K = k;
-        arr = new ArrayList<>();
-        for (int num : nums) {
-            arr.add(num);
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for(int num: nums) {
+            minHeap.offer(num);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
         }
     }
 
     public int add(int val) {
-        arr.add(val);
-        Collections.sort(arr);
-        return arr.get(arr.size() - K);
+        minHeap.offer(val);
+        if(minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
     }
 }
--- src/C10HeapPriorityQueue/P2LastStoneWeight/Solution.java
+++ src/C10HeapPriorityQueue/P2LastStoneWeight/Solution.java
@@ -1,4 +1,8 @@
 package C10HeapPriorityQueue.P2LastStoneWeight;
 
 public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Worlds");
+    }
 }
