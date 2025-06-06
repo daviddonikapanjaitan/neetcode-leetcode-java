5bafa775c09003c23dfee0246ea4f2fc916e5292
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-06-07 00:59:11 +0700

    add solutions for tree problems.

---
+++ src/C14Greedy/P3JumpGameII/Solution.java
@@ -0,0 +1,32 @@
+package C14Greedy.P3JumpGameII;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public Solution() {
+        int[] nums = new int[]{2, 3, 1, 1, 4};
+        // int[] nums = new int[]{2, 3, 0, 1, 4};
+        int result = jump(nums);
+
+        System.out.println(result);
+    }
+
+    public int jump(int[] nums) {
+        int res = 0, l = 0 , r = 0;
+
+        while (r < nums.length - 1) {
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            res++;
+        }
+
+        return res;
+    }
+}
