2d4614d3f29844e46a8f7f9c8e18d7c0d7869612
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-06-17 21:19:38 +0700

    add solutions for tree problems.

---
+++ src/Practice/C16D2DynamicProgramming/P11RegularExpressionMatching/Solution.java
@@ -0,0 +1,49 @@
+package Practice.C16D2DynamicProgramming.P11RegularExpressionMatching;
+
+public class Solution {
+    private Boolean[][] dp;
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public Solution() {
+        String s = "aa";
+        String p = "a";
+
+        // String s = "aa";
+        // String p = "a*";
+
+        // String s = "ab";
+        // String p = ".*";
+
+        boolean result = isMatch(s, p);
+        System.out.println(result);
+    }
+
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        dp = new Boolean[m + 1][n + 1];
+        return dfs(0, 0, s, p, m, n);
+    }
+
+    private boolean dfs(int i, int j, String s, String p, int m, int n) {
+        if(j == n) {
+            return i == m;
+        }
+        if(dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        boolean match = i < m && (s.charAt(i) == p.charAt(j) ||
+                p.charAt(j) == '.');
+        if(j + 1 < n && p.charAt(j + 1) == '*') {
+            dp[i][j] = dfs(i, j + 2, s, p, m, n) ||
+                    (match && dfs(i + 1, j, s, p, m, n));
+        } else {
+            dp[i][j] = match && dfs(i + 1, j + 1, s, p, m, n);
+        }
+
+        return dp[i][j];
+    }
+}
