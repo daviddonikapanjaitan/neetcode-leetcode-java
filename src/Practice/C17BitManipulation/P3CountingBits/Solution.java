6e6b4b3619cee050e93be187ad035444c342479b
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2025-06-17 21:41:10 +0700

    add solutions for tree problems.

---
+++ src/Practice/C17BitManipulation/P3CountingBits/Solution.java
@@ -0,0 +1,32 @@
+package Practice.C17BitManipulation.P3CountingBits;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public Solution() {
+        int input = 2;
+        // int input = 5;
+        int[] result = countBits(input);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        int offset = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if(offset * 2 == i) {
+                offset = i;
+            }
+            dp[i] = 1 + dp[i - offset];
+        }
+
+        return dp;
+    }
+}
